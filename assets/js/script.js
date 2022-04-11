691d31c50a3eab221d10f39fee60d29c3f44a4c7
nearcatch <692914+nearcatch@users.noreply.github.com>
2022-04-11 00:24:29 -0500

    basic parsing of weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,21 @@
 let apiOpenWeather = "9696e5041f086f68c81a5f683ef01308";
 let cityData;
 
-function getWeather(search) {
+function getWeather(city) {
   fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${search}&appid=${apiOpenWeather}
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiOpenWeather}
     `
   )
     .then((response) => response.json())
     .then((geoData) => {
       return fetch(
-        `https://api.openweathermap.org/data/2.5/onecall?lat=${geoData[0].lat}&lon=${geoData[0].lon}&appid=${apiOpenWeather}`
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${geoData[0].lat}&lon=${geoData[0].lon}&units=imperial&appid=${apiOpenWeather}`
       );
     })
     .then((response) => response.json())
     .then((data) => {
       cityData = data;
-      // console.log(data);
-      // weatherP.innerText = "City: " + data.city.name;
+      setWeatherVars(city,data);
     });
 }
 getWeather("New York City");
@@ -26,3 +25,16 @@ let cityWind;
 let cityHumidity;
 let cityUV;
 let cityForecast;
+
+function setWeatherVars(city,data) {
+  cityName = city
+  cityTemp = data.current.temp + " °F"
+  cityWind = data.current.wind_speed + " MPH"
+  cityHumidity = data.current.humidity + "%"
+  cityUV = data.current.uvi
+  cityForecast = data.daily
+  console.log(
+    `Name: ${cityName}, Temp: ${cityTemp}, Wind: ${cityWind}, Humidity: ${cityHumidity}, UV: ${cityUV}`
+  );
+}
+
