a152d14aa67afe1812124a23873cbdebf1936e26
nearcatch <692914+nearcatch@users.noreply.github.com>
2022-04-12 18:26:29 -0500

    css

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,11 @@ searchForm.addEventListener("submit", citySearch);
 function citySearch(event) {
   event.preventDefault();
   let cityName = searchInput.value.trim();
-  storeSearch(cityName);
-  getWeather(cityName);
+  console.log(typeof cityName.length)
+  if (Number(cityName.length) !== 0){
+    storeSearch(cityName);
+    getWeather(cityName);
+  }
   searchForm.reset();
 }
 
@@ -26,24 +29,36 @@ function getWeather(cityName) {
     .then((response) => response.json())
     .then((data) => {
       cityData = data;
-      writeWeather(data,cityName);
+      writeWeather(data, cityName);
     });
 }
 
-  function writeWeather(data,cityName) {
+function writeWeather(data, cityName) {
   let cityTemp = data.current.temp + " °F";
   let cityWind = data.current.wind_speed + " MPH";
   let cityHumidity = data.current.humidity + "%";
   let cityUV = data.current.uvi.toFixed(2);
-  let cityForecast = data.daily;
+  let uvColor = "uvPurple"
+  if (cityUV < 3) {
+    uvColor = "uvGreen";
+  } else if (cityUV < 6) {
+    uvColor = "uvYellow";
+  } else if (cityUV < 8) {
+    uvColor = "uvOrange";
+  } else if (cityUV < 11) {
+    uvColor = "uvRed";
+  }
+  let cityIcon = data.current.weather[0].icon;
   const currentInfo = document.querySelector("#cityDetails");
-  currentInfo.innerHTML = `<h2>Currently in ${cityName}</h2><p>Temp: ${cityTemp}</p><p>Wind: ${cityWind}</p><p>Humidity: ${cityHumidity}</p><p>UV Index: <span id="uv"> ${cityUV}</span></p>`;
+  currentInfo.innerHTML = `<h2>Currently in <span class="capitalCase">${cityName}</span></h2><img id="currentIcon" src="http://openweathermap.org/img/wn/${cityIcon}@2x.png" alt="a small icon depicting the current day's conditions"><p>Temp: ${cityTemp}</p><p>Wind: ${cityWind}</p><p>Humidity: ${cityHumidity}</p><p>UV Index: <span class="${uvColor}"> ${cityUV}</span></p>`;
+  let cityForecast = data.daily;
   const forecastDays = document.querySelector("#forecastDays");
   forecastDays.innerHTML = "";
   for (let i = 1; i < 6; i++) {
     let castDay = cityForecast[i];
     let castIcon = castDay.weather[0].icon;
-    let castTemp = `${castDay.temp.max} °F / ${castDay.temp.min} °F`;
+    let castTempHi = `${castDay.temp.max} °F`;
+    let castTempLow = `${castDay.temp.min} °F`;
     let castWind = castDay.wind_speed + " MPH";
     let castHumid = castDay.humidity + "%";
     let castDayDate = new Date(castDay.dt * 1000);
@@ -52,14 +67,28 @@ function getWeather(cityName) {
     let castDayDay = castDayDate.getDate();
     forecastDays.innerHTML += `<div class="daycast"><h4>${castDayYear}/${castDayMonth.slice(
       -2
-    )}/${castDayDay}</h4><img src="http://openweathermap.org/img/wn/${castIcon}@2x.png" alt="a small icon depicting the forecasted day's conditions"><p>Temp: ${castTemp}</p><p>Wind: ${castWind}</p><p>Humidity: ${castHumid}</p></div>`;
+    )}/${castDayDay}</h4><img src="http://openweathermap.org/img/wn/${castIcon}@2x.png" alt="a small icon depicting the forecasted day's conditions"><p>High: ${castTempHi}</p><p>Low: ${castTempLow}</p><p>Wind: ${castWind}</p><p>Humidity: ${castHumid}</p></div>`;
   }
 }
 
 // set weatherSearches from localStorage
 let weatherSearches = JSON.parse(localStorage.getItem("weatherSearches"));
+const previousSearches = document.querySelector("#previousSearches");
 if (weatherSearches === null) {
   weatherSearches = [];
+} else {
+  getWeather(weatherSearches[0]);
+  for (let i = 0; i < 8; i++) {
+    previousSearches.innerHTML += `<button class="capitalCase">${weatherSearches[i]}</button>`;
+  }
+  previousSearches.addEventListener("click", prevSearch);
+}
+function prevSearch(event) {
+  if (event.target.matches("button")) {
+    prevCityName = event.target.innerText;
+    storeSearch(prevCityName);
+    getWeather(prevCityName);
+  }
 }
 function storeSearch(search) {
   // current search lowercased and stored as var
